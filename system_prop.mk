d4b7e7dd60ad01222849ad07bf3f4655e7bef44a
UtsavisGreat <utsavbalar1231@gmail.com>
2019-11-23 17:55:06 +0000

    Davinci: update MM props from CAF

--- system_prop.mk
+++ system_prop.mk
@@ -97,6 +97,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/system/etc/media_profiles_vendor.xml
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true \
+    mm.enable.smoothstreaming=true \
+    vendor.mm.enable.qcom_parser=63963135
 
 # set lmkd options
 PRODUCT_PROPERTY_OVERRIDES += \
