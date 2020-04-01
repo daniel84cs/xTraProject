a1473425bc245e423bb05fd606967d9889df16f4
Marco <marco.rizzo.00@outlook.com>
2020-04-01 11:44:47 +0200

    davinci: props: import display properties from raphael

--- system_prop.mk
+++ system_prop.mk
@@ -39,11 +39,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.eyecare.brightness.threshold=11 \
+    ro.eyecare.brightness.level=5 \
+    ro.displayfeature.histogram.enable \
+    ro.hist.brightness.threshold=7 \
     ro.sf.lcd_density=440 \
     ro.vendor.display.ad=0 \
     ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
-    ro.vendor.display.sensortype=2
+    ro.vendor.display.sensortype=2 \
+    ro.xiaomi.bl.poll=true \
+    persist.displayfeature.dc_backlight.threshold=610 \
+    persist.displayfeature.dc_backlight.enable=false \
+    persist.fod.modified.dc_status=false \
+    sys.displayfeature.hbm.enable=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
